a5ebb6990709a739f425b43bbe21dca414f1059b
Navaneeth Malingan <navneetnivu07@gmail.com>
2018-08-06 18:55:59 +0530

    ESP Pin map and Data.txt

---
+++ esp32_pin_map.cpp
@@ -0,0 +1,26 @@
+
+
+int D15 = 15; //S0
+int D2 = 2;   //S1
+int D4 = 4;   //S2
+int D5 = 5;   //S3
+
+int D18 = 18; //tc1 SO
+int D19 = 19; //tc1 CS
+int D21 = 21; //tc1 SCK
+
+int D22 = 22;
+int D23 = 23;
+
+int D13 = 13;
+int D12 = 12;
+
+int D14 = 14;
+int D27 = 27;
+int D26 = 26;
+int D25 = 25;
+int D33 = 33;
+
+int D32 = 32; //tc2 SCK
+int D35 = 35; //tc2 CS
+int D34 = 34; //tc3 SO
